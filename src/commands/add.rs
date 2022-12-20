75f0ea65a0ea21273103b6b6529580b73489bba7
Illia Gorbatiuk <ilya.gorbatiuk@gmail.com>
2022-12-20 17:03:31 +0200

    AddCommand now uses the BibTeX parser

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -4,34 +4,14 @@ use super::{common::Book, ShishoCommand};
 
 pub struct AddCommand;
 
-impl Book {
-    fn parse(data: &[String]) -> Result<Self, &str> {
-        todo!()
-
-        // if data.len() != 3 {
-        //     return Err("Incorrent number of parameters");
-        // }
-
-        // let (name, authors, year_str) = (&data[0], &data[1], &data[2]);
-
-        // let book_year = u32::from_str_radix(year_str.as_str(), 10)
-        //     .map_err::<&str, _>(|_| "Cannot parse book year")?;
-
-        // return Ok(Book {
-        //     name: name.to_string(),
-        //     authors: authors.to_string(),
-        //     year: book_year,
-        // });
-    }
-}
-
 impl ShishoCommand for AddCommand {
     fn perform(args: &[String], stream: &mut std::net::TcpStream) {
-        let parsing_result = Book::parse(args);
+        let input = args.join("");
+        let parsing_result = Book::from_bibtex(input.as_str());
         let response = {
             match parsing_result {
                 Ok(book) => ApiResponse::ok_from(book.to_string().as_str()),
-                Err(error) => ApiResponse::error_from(error),
+                Err(error) => ApiResponse::error_from(error.as_str()),
             }
         };
         response.send_to_stream(stream);
